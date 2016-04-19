951ac1bb36e93b819eef8dc220e869167e3aada7
Mat Megat <matmegat822@gmail.com>
2016-04-19 13:53:04 +0300

    Feed: dummy post.

--- src/api/Feed.js
+++ src/api/Feed.js
@@ -9,8 +9,62 @@ module.exports = function Feed ()
 
 	feed.express.get('/latest', (rq, rs) =>
 	{
-		rs.json({ ok: true })
+		var dummy = [ dummy_post() ]
+
+		rs.json(dummy)
 	})
 
 	return feed
+}
+
+function dummy_post ()
+{
+	var response =
+	{
+		id: 1,
+		timestamp: dummy_date(),
+
+		investor:
+		{
+			id: 1,
+			name: 'Allen Schwartzh',
+			pic: null
+		},
+
+		event:
+		{
+			type: 'Trade',
+			data: // data is related to `type` and depends on it
+			{
+				dir: 'sold',
+
+				symbol:
+				{
+					ticker: 'ZG',
+					company: 'ZILLOW',
+				},
+
+				price: 150,
+				amount: 100,
+
+				text: 'I want to sell all my assets and become a downshifter.',
+
+				risk: 'low', // low|medium|high
+				motivations:
+				[
+					'Earnings', // maybe this will be expanded to motivation Objects
+					'Valuation',
+					'Growth Potential'
+				]
+			}
+		}
+	}
+
+	return response
+}
+
+
+function dummy_date ()
+{
+	return (new Date).toISOString()
 }
