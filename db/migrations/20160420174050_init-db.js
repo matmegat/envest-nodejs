784449224936e7bc4134600bc21fe9d2882f7a74
Mat Megat <matmegat822@gmail.com>
2016-04-20 23:24:31 +0300

    DB: workon migration infrastructure.

--- db/migrations/20160420174050_init-db.js
+++ db/migrations/20160420174050_init-db.js
@@ -1,10 +1,38 @@
 
 exports.up = function (knex, Promise)
 {
+	/* eslint-disable newline-per-chained-call */
 	return Promise.resolve()
+	.then(() =>
+	{
+		return knex.schema.createTable('users', (table) =>
+		{
+			table.increments('id').primary()
+
+			table.string('first_name').notNullable()
+			table.string('last_name').notNullable()
+
+			table.string('email')
+			table.string('password', 36).notNullable()
+			table.string('salt', 16).notNullable()
+		})
+	})
+	.then(() =>
+	{
+		return knex.schema.createTable('email_confirms', (table) =>
+		{
+			table.string('new_email').primary()
+			table.string('code', 16).notNullable()
+		})
+	})
+	/* eslint-enable newline-per-chained-call */
 }
 
 exports.down = function (knex, Promise)
 {
-	return Promise.resolve()
+	return Promise.all(
+	[
+		knex.schema.dropTableIfExists('users'),
+		knex.schema.dropTableIfExists('email_confirms'),
+	])
 }
