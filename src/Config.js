4f1f868d0af8fb700e924fee8f7af0a0e6e0963c
Mat Megat <matmegat822@gmail.com>
2016-04-20 15:20:39 +0300

    use config.json and (optional) dev.json on top of it.

--- src/Config.js
+++ src/Config.js
@@ -1,6 +1,4 @@
 
-var load = require('fs-sync').readJSON
-
 var Config = module.exports = function Config (app)
 {
 	var root = app.root.partial('cfg')
@@ -10,14 +8,28 @@ var Config = module.exports = function Config (app)
 	return cfg
 }
 
+
+var fs = require('fs-sync')
+var load = fs.readJSON
+var exists = fs.exists
+
+var merge = require('lodash/merge')
+
 Config.process = function (cfg_rootpath)
 {
-	var cfg = load(cfg_rootpath('dev.json'))
+	var cfg = load(cfg_rootpath('config.json'))
 
-	if (cfg.dev)
+	var dev = cfg_rootpath('dev.json')
+
+	if (exists(dev))
 	{
-		console.info('running dev config')
+		console.info('applying dev config')
+
+		cfg = merge({}, cfg, load(dev))
 	}
+
+	console.info('config: env = %s', cfg.env)
+	console.info('config: dev = %s', cfg.dev)
 
 	return cfg
 }
