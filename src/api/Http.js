7997edadd174c55ad48516c82d4d426b342d4538
Mat Megat <matmegat822@gmail.com>
2016-04-25 16:52:00 +0300

    Auth: include auth-module into http-module, set body-parser as middleware

--- src/api/Http.js
+++ src/api/Http.js
@@ -1,16 +1,22 @@
 
 var express = require('express')
+var body_parser = require('body-parser')
 
 var Feed = require('./Feed')
+var Auth = require('./Auth')
 
 module.exports = function Http (app)
 {
 	var http = {}
 
 	http.express = express()
+	http.express.use(body_parser.json())
 
 	http.feed = Feed()
 	http.express.use('/api/feed', http.feed.express)
+
+	http.auth = Auth(app.db)
+	http.express.use('/api/auth', http.auth.express)
 
 	var port = app.cfg.port
 
