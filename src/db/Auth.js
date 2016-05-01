e5eb94dc9c9823cf428c198f5e1e7afb2caa2956
Mat Megat <matmegat822@gmail.com>
2016-05-01 20:43:42 +0300

    Db/Auth: compare_passwords is purely private.

--- src/db/Auth.js
+++ src/db/Auth.js
@@ -34,7 +34,7 @@ module.exports = function Auth (db)
 		{
 			if (user)
 			{
-				return auth.comparePasswords(user.password, password, user.salt)
+				return compare_passwords(user.password, password, user.salt)
 				.then(result =>
 				{
 					if (result)
@@ -63,15 +63,6 @@ module.exports = function Auth (db)
 		})
 	}
 
-	auth.comparePasswords = function (dbPass, formPass, salt)
-	{
-		return encrypt_pass(formPass, salt)
-		.then(result =>
-		{
-			return result.encrypted_pass === dbPass
-		})
-	}
-
 	return auth
 }
 
@@ -96,12 +87,6 @@ function generate_salt ()
 	.then(hex)
 }
 
-function hash (password, salt)
-{
-	return genHash(password, salt, iterations, password_size, 'sha512')
-	.then(hex)
-}
-
 function encrypt_pass (password, salt)
 {
 	return hash(password, '', password_size)
@@ -115,5 +100,20 @@ function encrypt_pass (password, salt)
 			encrypted_pass: str,
 			salt: salt
 		}
+	})
+}
+
+function hash (password, salt)
+{
+	return genHash(password, salt, iterations, password_size, 'sha512')
+	.then(hex)
+}
+
+function compare_passwords (dbPass, formPass, salt)
+{
+	return encrypt_pass(formPass, salt)
+	.then(result =>
+	{
+		return result.encrypted_pass === dbPass
 	})
 }
