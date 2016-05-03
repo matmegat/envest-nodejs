1ab1d787965e430794015995745d6b12cdf2cea0
Mat Megat <matmegat822@gmail.com>
2016-05-03 14:39:45 +0300

    simplify email regex.

--- src/db/Auth.js
+++ src/db/Auth.js
@@ -148,7 +148,7 @@ function encrypt_pass (password, salt)
 
 function validate (credentials)
 {
-	var emailRe = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+	var emailRe = /@/
 
 	return new Promise((resolve, reject) =>
 	{
