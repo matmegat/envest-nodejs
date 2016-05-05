c14fc3b003601263b2d32871ceacf44117d231bf
Mat Megat <matmegat822@gmail.com>
2016-05-05 15:16:45 +0300

    (NET-470) Add comments to schema migration

--- db/migrations/20160505104748_feed.js
+++ db/migrations/20160505104748_feed.js
@@ -1,9 +1,11 @@
 
-exports.up = function (knex, Promise) {
+exports.up = function (knex, Promise)
+{
 	return Promise.resolve()
 		.then(() =>
 		{
 			console.info('Create Table for Motivations')
+
 			return knex.schema.createTable('motivations', (table) =>
 			{
 				table.increments('id').primary()
@@ -17,14 +19,14 @@ exports.up = function (knex, Promise) {
 		{
 			console.info('Create Table for Feed')
 
-			return knex.schema.createTable('feed_items', (table) =>
+			return knex.schema.createTable('posts', (table) =>
 			{
 				table.increments('id').primary()
 
 				table.timestamp('timestamp').defaultTo(knex.fn.now())
 				table.integer('invetor_id').notNullable()
 				// TODO: update on adding table for Investor
-				// table.Foreign('invetor_id').references('investor.id')
+				// table.foreign('invetor_id').references('investor.id')
 				table.jsonb('event').notNullable()
 				/* TODO: should follow notation
 				* {
@@ -43,12 +45,31 @@ exports.up = function (knex, Promise) {
 				* */
 			})
 		})
+		.then(() =>
+		{
+			console.info('Create Table for Comments')
+
+			return knex.schema.createTable('comments', (table) =>
+			{
+				table.increments('id').primary()
+
+				table.integer('user_id').notNullable()
+				table.foreign('user_id').references('users.id')
+
+				table.integer('post_id').notNullable()
+				table.foreign('post_id').references('posts.id')
+
+				table.text('text').notNullable()
+			})
+		})
 }
 
-exports.down = function (knex, Promise) {
+exports.down = function (knex, Promise)
+{
 	return Promise.all(
 	[
-		knex.schema.dropTableIfExists('feed_items'),
+		knex.schema.dropTableIfExists('comments'),
+		knex.schema.dropTableIfExists('posts'),
 		knex.schema.dropTableIfExists('motivations')
 	])
 }
