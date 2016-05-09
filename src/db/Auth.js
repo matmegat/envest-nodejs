160772f2406f8d83c6403c53928e58d6c2bd0277
Mat Megat <matmegat822@gmail.com>
2016-05-09 19:12:20 +0300

    Fix Auth salt generation

--- src/db/Auth.js
+++ src/db/Auth.js
@@ -106,9 +106,9 @@ module.exports = function Auth (db)
 
 
 // DB salt size = 8 chars (16 bytes), DB password size = 18 chars (36 bytes)
-var salt_size     = 16 / 2
-var code_size     = 16 / 2
-var password_size = 36 / 2
+var salt_size     = 16
+var code_size     = 16
+var password_size = 36
 var iterations    = 100000
 
 var promisify = require('promisify-node')
