f651b7ab120bdb38899938605ad0dc0d3b67bab0
Mat Megat <matmegat822@gmail.com>
2016-05-16 17:45:35 +0300

    (NET-463)(NET-501) Add aliases of comments to feed

--- src/http/api/feed/Feed.js
+++ src/http/api/feed/Feed.js
@@ -38,6 +38,26 @@ module.exports = function Feed (db)
 		toss(rs, feed.model.byId(rq.params.id))
 	})
 
+	feed.express.get('/:id/comments', (rq, rs) =>
+	{
+		var options = _.pick(rq.query,
+		[
+			'max_id',
+			'since_id',
+		])
+		options.feed_id = rq.params.id
+
+		toss(rs, db.comments.list(options))
+	})
+
+	feed.express.post('/:id/comments', (rq, rs) =>
+	{
+		var comment_data = _.pick(rq.body, [ 'text' ])
+		comment_data.feed_id = rq.params.id
+
+		toss(rs, db.comments.create(comment_data))
+	})
+
 	return feed
 }
 
