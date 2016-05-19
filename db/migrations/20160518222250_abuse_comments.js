510813f851db568fdc16289e944384ae340a8625
Mat Megat <matmegat822@gmail.com>
2016-05-19 20:54:04 +0300

    (NET-544) added links to users.id and comments.id

--- db/migrations/20160518222250_abuse_comments.js
+++ db/migrations/20160518222250_abuse_comments.js
@@ -4,9 +4,17 @@ exports.up = function (knex)
 	return knex.schema.createTable('abuse_comments', (table) =>
 	{
 		table.integer('user_id')
+			.references('users.id')
+			.onUpdate('restrict')
+			.onDelete('restrict')
+
 		table.integer('comment_id')
+			.references('comments.id')
+			.onUpdate('restrict')
+			.onDelete('restrict')
+
 		table.timestamp('timestamp').defaultTo(knex.fn.now())
-		table.primary(['user_id', 'comment_id'])
+		table.primary([ 'user_id', 'comment_id' ])
 	})
 }
 
