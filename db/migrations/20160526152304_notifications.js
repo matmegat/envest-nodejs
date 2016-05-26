ef90ba73ec8040660299bc14fd19ca0bb5833422
Mat Megat <matmegat822@gmail.com>
2016-05-26 22:06:35 +0300

    (NET-555) add migration

---
+++ db/migrations/20160526152304_notifications.js
@@ -0,0 +1,46 @@
+
+exports.up = function (knex, Promise)
+{
+	return Promise.resolve()
+	.then(() =>
+	{
+		return knex.schema.createTable('notifications', (table) =>
+		{
+			table.increments('id').primary()
+
+			table.timestamp('timestamp').defaultTo(knex.fn.now())
+
+			table.string('type').notNullable()
+			table.jsonb('event').notNullable()
+
+			table.integer('recipient_id')
+				.references('users.id')
+				.onUpdate('cascade')
+				.onDelete('cascade')
+
+		})
+	})
+	.then(() =>
+	{
+		return knex.schema.createTable('notifications_viewed', (table) =>
+		{
+			table.integer('recipient_id')
+				.primary()
+				.references('users.id')
+				.onUpdate('cascade')
+				.onDelete('cascade')
+
+			table.integer('last_viewed_id').notNullable()
+
+		})
+	})
+}
+
+exports.down = function (knex, Promise)
+{
+	return Promise.all(
+	[
+		knex.schema.dropTableIfExists('notifications'),
+		knex.schema.dropTableIfExists('notifications_viewed'),
+	])
+}
