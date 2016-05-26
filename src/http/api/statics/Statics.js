c650e070f9083304eb75b96633c58de776e39727
Mat Megat <matmegat822@gmail.com>
2016-05-26 15:04:42 +0300

    Add dynamic mime-type detection

--- src/http/api/statics/Statics.js
+++ src/http/api/statics/Statics.js
@@ -2,6 +2,7 @@
 var Router = require('express').Router
 
 var stream = require('fs').createReadStream
+var mime = require('mime')
 
 module.exports = function Statics (rootpath)
 {
@@ -14,8 +15,16 @@ module.exports = function Statics (rootpath)
 	{
 		console.log('Getting image by hash: `%s`', rq.params.hash)
 
-		rs.setHeader('content-type', 'image/png')
-		stream(filename).pipe(rs)
+		Promise.resolve()
+		.then(() =>
+		{
+			return mime.lookup(filename)
+		})
+		.then(type =>
+		{
+			rs.setHeader('content-type', type)
+			stream(filename).pipe(rs)
+		})
 	})
 
 	return statics
