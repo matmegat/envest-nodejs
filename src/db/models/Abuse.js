2cad9c2f3e4292d20427fc58252d4f33cdaccaac
Mat Megat <matmegat822@gmail.com>
2016-05-27 15:51:12 +0300

    fix abuse on your comments

--- src/db/models/Abuse.js
+++ src/db/models/Abuse.js
@@ -13,19 +13,25 @@ module.exports = function Abuse (db, comments)
 
 	var CommentNotExist = Err('comment_not_exist', 'Comment not exist')
 	var AbuseExist = Err('abuse_exist', 'Abuse exist')
+	var YourComments = Err('is_your_comment', 'It is impossible to abuse for your comments')
 
 	abuse.create = function (user_id, comment_id)
 	{
 		return comments.byId(comment_id)
 		.then(Err.nullish(CommentNotExist))
-		.then(() =>
+		.then((comment_items) =>
 		{
-			return abuse.table()
-			.insert({
-				user_id: user_id,
-				comment_id: comment_id
-			})
-			.then(noop)
+			if (comment_items.user_id != user_id)
+			{
+				return abuse.table()
+				.insert({
+					user_id: user_id,
+					comment_id: comment_id
+				})
+				.then(noop)
+			}
+
+			throw YourComments()
 		})
 		.catch(Err.fromDb('abuse_comments_pkey', AbuseExist))
 	}
