77f58fe43c55877567ad3f7ebc58fea8b074ef7f
Mat Megat <matmegat822@gmail.com>
2016-05-27 15:06:59 +0300

    Admin: ensure, move auth-required logic to model.

--- src/db/models/Admin.js
+++ src/db/models/Admin.js
@@ -1,6 +1,10 @@
 
 var knexed = require('../knexed')
 
+var Err = require('../../Err')
+var AdminRequired =
+    Err('admin_required', 'Admin privileges is required for this operation')
+
 var expect = require('chai').expect
 
 module.exports = function Admin (db)
@@ -14,6 +18,33 @@ module.exports = function Admin (db)
 
 	var user = db.user
 	expect(db, 'Admin depends on User').property('user')
+
+
+	admin.ensure = function (user_id)
+	{
+		/*
+		 * if admin.is() throws by any reason we cast it to `false`
+		 * any `false` by any mean leads to AdminRequired error
+		 *
+		 * so we guarantee that admin-required is OK ONLY if
+		 * admin.is() is `true`
+		 */
+		return admin.is(user_id)
+
+		// if any error occurs, cast them to false
+		.catch(debug)
+
+		// capture all falsy values
+		.then(Err.falsy(AdminRequired))
+	}
+
+	function debug (error)
+	{
+		console.error('admin-required error')
+		console.error(error)
+
+		return false
+	}
 
 	admin.is = function (user_id)
 	{
--- src/http/admin-required.js
+++ src/http/admin-required.js
@@ -1,39 +1,11 @@
 
-var Err = require('../Err')
-var AdminRequired =
-    Err('admin_required', 'Admin privileges is required for this operation')
-
 var toss = require('./toss')
 
 module.exports = function (admin)
 {
 	return (rq, rs, next) =>
 	{
-		/*
-		 * if admin.is() throws by any reason we cast it to `false`
-		 * any `false` by any mean leads to AdminRequired error
-		 *
-		 * so we guarantee that admin-required is OK ONLY if
-		 * admin.is() is `true`
-		 */
-
-		return admin.is(rq.user.id)
-
-		// if any error occurs, cast them to false
-		.catch(debug)
-
-		// capture all falsy values
-		.then(Err.falsy(AdminRequired))
-
-		//    OK            ERR
+		return admin.ensure(rq.user.id)
 		.then(() => next(), toss.err(rs))
 	}
-}
-
-function debug (error)
-{
-	console.error('admin-required error')
-	console.error(error)
-
-	return false
 }
