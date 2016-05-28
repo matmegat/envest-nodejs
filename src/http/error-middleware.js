a7f1ccec74c049733b52018b33fe9ef26d78dbbe
Mat Megat <matmegat822@gmail.com>
2016-05-28 18:08:54 +0300

    allow middleware error handler to have 4 parameters.

--- src/http/error-middleware.js
+++ src/http/error-middleware.js
@@ -4,7 +4,7 @@ var Err = require('../Err')
 
 var MiddlewareError = Err('middleware_error', 'Middleware error')
 
-// eslint-disable-next-line no-unused-vars
+// eslint-disable-next-line no-unused-vars, max-params
 module.exports = (err, rq, rs, next) =>
 {
 	if (Err.is(err))
