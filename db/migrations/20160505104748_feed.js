bc4e7d38883e6d46f68af369c60f12b98076ea99
Mat Megat <matmegat822@gmail.com>
2016-06-01 12:28:34 +0300

    get rid of investors_migration file

--- db/migrations/20160505104748_feed.js
+++ db/migrations/20160505104748_feed.js
@@ -1,13 +1,35 @@
-var Investors = require('./../investor_migration')
+var investorProfilePic = '/api/static/pic/b4f18f5b05307bd1e3cc00e0802d641b'
 
 exports.up = function (knex, Promise)
 {
-	var investor_migration = Investors(knex)
-
 	return Promise.resolve()
 		.then(() =>
 		{
-			return investor_migration.initialUp
+			return knex.schema.createTable('investors', (table) =>
+			{
+				table.integer('user_id').primary().unique().notNullable()
+				.references('users.id')
+				.onUpdate('restrict') /* user.id should never change */
+				.onDelete('restrict')
+
+				table.string('first_name').notNullable()
+				table.string('last_name').notNullable()
+				table.text('profile_pic').defaultTo(investorProfilePic)
+				table.string('profession').defaultTo('')
+				table.jsonb('focus').defaultTo('[]')
+				table.text('background').defaultTo('')
+				table.jsonb('historical_returns').notNullable().defaultTo(
+					JSON.stringify(
+						[
+							{ year: 2011, percentage: 10 },
+							{ year: 2012, percentage: 11 },
+							{ year: 2013, percentage: -8 },
+							{ year: 2014, percentage: 5 },
+							{ year: 2015, percentage: 15 },
+						])
+				)
+				table.boolean('is_public').defaultTo(false)
+			})
 		})
 		.then(() =>
 		{
@@ -59,12 +81,10 @@ exports.up = function (knex, Promise)
 
 exports.down = function (knex, Promise)
 {
-	var investor_migration = Investors(knex)
-
 	return Promise.all(
 	[
 		knex.schema.dropTableIfExists('comments'),
 		knex.schema.dropTableIfExists('feed_items'),
-		investor_migration.initialDown
+		knex.schema.dropTableIfExists('investors')
 	])
 }
