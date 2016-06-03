3c86e5e24e6a66f17c3eb67c50b4d3d918469520
Mat Megat <matmegat822@gmail.com>
2016-06-03 15:14:01 +0300

    debug: show hostname.

--- src/http/ReqLog.js
+++ src/http/ReqLog.js
@@ -3,6 +3,8 @@ module.exports = function (log, express)
 {
 	express.use('/api', (rq, rs, next) =>
 	{
+		console.log(rq.hostname)
+
 		log('%s %s\n%j', rq.method, rq.originalUrl, rq.body)
 		next()
 	})
