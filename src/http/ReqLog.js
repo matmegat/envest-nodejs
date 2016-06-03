c77a114cdf50c876343e5178159a00582bd6cb26
Mat Megat <matmegat822@gmail.com>
2016-06-03 15:16:37 +0300

    debug: show Content-Type to catch various client-s types.

--- src/http/ReqLog.js
+++ src/http/ReqLog.js
@@ -4,6 +4,7 @@ module.exports = function (log, express)
 	express.use('/api', (rq, rs, next) =>
 	{
 		console.log(rq.hostname)
+		console.log(rq.get('Content-Type'))
 
 		log('%s %s\n%j', rq.method, rq.originalUrl, rq.body)
 		next()
