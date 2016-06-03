6214ee14fe79c84345eddf36e687da8b9be25228
Mat Megat <matmegat822@gmail.com>
2016-06-03 15:20:18 +0300

    debug: arrange output.

--- src/http/ReqLog.js
+++ src/http/ReqLog.js
@@ -3,10 +3,11 @@ module.exports = function (log, express)
 {
 	express.use('/api', (rq, rs, next) =>
 	{
+		log('%s %s\n%j', rq.method, rq.originalUrl, rq.body)
+
 		console.log(rq.hostname)
 		console.log(rq.get('Content-Type'))
 
-		log('%s %s\n%j', rq.method, rq.originalUrl, rq.body)
 		next()
 	})
 }
