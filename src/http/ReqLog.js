0eaf138bdea6b19b5ea61763645856084728c960
Mat Megat <matmegat822@gmail.com>
2016-06-07 14:29:34 +0300

    ReqLog: fade secret info from text output.

--- src/http/ReqLog.js
+++ src/http/ReqLog.js
@@ -3,8 +3,21 @@ module.exports = function (log, express)
 {
 	express.use('/api', (rq, rs, next) =>
 	{
-		log('%s %s\n%j', rq.method, rq.originalUrl, rq.body)
+		var body = rq.body
+
+		if (rq.originalUrl in fade)
+		{
+			body = Object.assign({}, body, { password: '*' })
+		}
+
+		log('%s %s\n%j', rq.method, rq.originalUrl, body)
 
 		next()
 	})
+}
+
+var fade =
+{
+	'/api/auth/register': 1,
+	'/api/auth/login': 1
 }
