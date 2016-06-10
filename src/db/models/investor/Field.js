e81f651c12ffc0f67224617ffeecca1908b7acbf
Mat Megat <matmegat822@gmail.com>
2016-06-10 15:07:46 +0300

    Field first approach.

---
+++ src/db/models/investor/Field.js
@@ -0,0 +1,51 @@
+
+module.exports = function Field (options)
+{
+	options = extend({}, defaults, options)
+
+	expect(options.set || options.name, '`set` or field `name` required').ok
+
+	var field = {}
+
+	field.validate = validator(options.validate)
+
+	return field
+}
+
+var extend = require('lodash/extend')
+var expect = require('chai').expect
+
+var same   = require('lodash/identity')
+
+var defaults =
+{
+	validate: same,
+
+	get: null,
+	set: null,
+	name: null
+}
+
+/*function converter (convert)
+{
+	if (typeof convert !== 'function')
+	{
+		convert = same
+	}
+
+	return (value) => convert(value)
+}
+*/
+
+function validator (validate)
+{
+	if (typeof validate !== 'function')
+	{
+		validate = same
+	}
+
+	return (value) =>
+	{
+		return new Promise(rs => rs(validate(value)))
+	}
+}
