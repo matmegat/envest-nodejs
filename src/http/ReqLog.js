40d11f4f0ec8b3f5b55d43a484871765b75fd647
Mat Megat <matmegat822@gmail.com>
2016-06-21 16:20:31 +0300

    ReqLog: improve password fading.

--- src/http/ReqLog.js
+++ src/http/ReqLog.js
@@ -1,4 +1,6 @@
 
+var map = require('lodash/mapValues')
+
 module.exports = function (log, express)
 {
 	express.use('/api', (rq, rs, next) =>
@@ -7,7 +9,17 @@ module.exports = function (log, express)
 
 		if (rq.originalUrl in fade)
 		{
-			body = Object.assign({}, body, { password: '*' })
+			body = map(body, (value, key) =>
+			{
+				if (key in secrets)
+				{
+					return '*'
+				}
+				else
+				{
+					return value
+				}
+			})
 		}
 
 		log('%s %s\n%j', rq.method, rq.originalUrl, body)
@@ -19,5 +31,14 @@ module.exports = function (log, express)
 var fade =
 {
 	'/api/auth/register': 1,
-	'/api/auth/login': 1
+	'/api/auth/login': 1,
+	'/api/password/reset': 1,
+	'/api/password/change': 1
+}
+
+var secrets =
+{
+	'password': 1,
+	'pass': 1,
+	'new_pass': 1
 }
