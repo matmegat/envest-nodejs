50abf7f2098d753951070dee49371531b2d3fa2b
Mat Megat <matmegat822@gmail.com>
2016-06-23 17:17:50 +0300

    fix name validation: allow dot at the end of every word, not just last. rm dot from the mid of word.

--- src/db/validate.js
+++ src/db/validate.js
@@ -86,9 +86,9 @@ validate.name = function validate__name (name, field_name)
 
 	   Should begin with a letter and end with a letter or dot.
 	*/
-	var re = XRegExp.build(`^ {{word}} (\\s {{word}})? \\.? $`,
+	var re = XRegExp.build(`^ {{word}} (\\s {{word}})? $`,
 	{
-		word: XRegExp(`\\pL+ ([. ' -] \\pL+)*`, 'x')
+		word: XRegExp(`\\pL+ ([' -] \\pL+)* \\.?`, 'x')
 	},
 	'x')
 
