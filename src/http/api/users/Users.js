34e575a10a398f971de1299782c74920ec42b205
Mat Megat <matmegat822@gmail.com>
2016-06-27 14:29:28 +0300

    Lock users routes with auth_required

--- src/http/api/users/Users.js
+++ src/http/api/users/Users.js
@@ -2,12 +2,14 @@
 var Router = require('express').Router
 
 var toss = require('../../toss')
+var authRequired = require('../../auth-required')
 
 module.exports = function Users (user_model)
 {
 	var users = {}
 
 	users.express = Router()
+	users.express.use(authRequired)
 	users.model = user_model
 
 	users.express.get('/:id', (rq, rs) =>
