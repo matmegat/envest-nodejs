11c50efa8e107d9a6d643fac584b221dc4d0ed59
Mat Megat <matmegat822@gmail.com>
2016-06-28 14:08:49 +0300

    construct uri pass to xignite.

--- src/db/models/symbols/Xign.js
+++ src/db/models/symbols/Xign.js
@@ -1,6 +1,6 @@
 
-var format = require('util').format
-
+var format = require('url').format
+var parse  = require('url').parse
 
 var expect = require('chai').expect
 
@@ -18,7 +18,35 @@ module.exports = function Xign (cfg)
 
 	X.fundamentals = (symbol) =>
 	{
-		return request('https://factsetfundamentals.xignite.com/xFactSetFundamentals.json/GetFundamentals?IdentifierType=Symbol&Identifiers=GOOG&FundamentalTypes=MarketCapitalization,BookValue,CEO&AsOfDate=6/27/2016&ReportType=Annual&ExcludeRestated=false&UpdatedSince='+'&_Token='+token)
+		var uri = format({
+protocol: 'https:',
+  // slashes: true,
+  // auth: null,
+  host: 'factsetfundamentals.xignite.com',
+  // port: null,
+  // hostname: 'factsetfundamentals.xignite.com',
+  // hash: null,
+  // search: '?IdentifierType=Symbol&Identifiers=GOOG&FundamentalTypes=MarketCapitalization,BookValue,CEO&AsOfDate=6/27/2016&ReportType=Annual&ExcludeRestated=false&UpdatedSince=&_Token=5198053EF65A4DE691F4D622001DEE79',
+  query: 
+   { IdentifierType: 'Symbol',
+     Identifiers: 'GOOG',
+     FundamentalTypes: 'MarketCapitalization,BookValue,CEO',
+     AsOfDate: '6/27/2016',
+     ReportType: 'Annual',
+     ExcludeRestated: 'false',
+     UpdatedSince: '',
+     _Token: '5198053EF65A4DE691F4D622001DEE79' },
+  pathname: '/xFactSetFundamentals.json/GetFundamentals',
+  // path: '/xFactSetFundamentals.json/GetFundamentals?IdentifierType=Symbol&Identifiers=GOOG&FundamentalTypes=MarketCapitalization,BookValue,CEO&AsOfDate=6/27/2016&ReportType=Annual&ExcludeRestated=false&UpdatedSince=&_Token=5198053EF65A4DE691F4D622001DEE79',
+  // href: 'https://factsetfundamentals.xignite.com/xFactSetFundamentals.json/GetFundamentals?IdentifierType=Symbol&Identifiers=GOOG&FundamentalTypes=MarketCapitalization,BookValue,CEO&AsOfDate=6/27/2016&ReportType=Annual&ExcludeRestated=false&UpdatedSince=&_Token=5198053EF65A4DE691F4D622001DEE79'
+	})
+
+		console.log(uri)
+
+		// var uri = parse('https://factsetfundamentals.xignite.com/xFactSetFundamentals.json/GetFundamentals?IdentifierType=Symbol&Identifiers=GOOG&FundamentalTypes=MarketCapitalization,BookValue,CEO&AsOfDate=6/27/2016&ReportType=Annual&ExcludeRestated=false&UpdatedSince='+'&_Token='+token, true)
+		return request(uri)
+
+		return Promise.resolve()
 	}
 
 	return X
