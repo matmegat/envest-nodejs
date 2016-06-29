405d1f4a43541573dca51d4f9ba96a04015e6d4e
Mat Megat <matmegat822@gmail.com>
2016-06-30 01:01:06 +0300

    Mailer: use config for API key.

---
+++ src/Mailer.js
@@ -0,0 +1,76 @@
+
+var _ = require('lodash')
+
+var SendGrid = require('sendgrid')
+
+var Err = require('./Err')
+var SendgridError = Err('sendgrid_error', 'Sendgrid error.')
+
+module.exports = function Mailer (cfg)
+{
+	var mailer = {}
+
+	var sendgrid = SendGrid(cfg.key)
+
+	var defaults =
+	{
+		subject: 'NetVest Mailer',
+		text: 'NetVest Mailer',
+		html: 'NetVest Mailer',
+		from: 'netvest.mailer@gmail.com',
+		fromname: 'NetVest'
+	}
+
+	var templates =
+	{
+		welcome: '5ef32fb6-9c55-416b-ae81-0d2df70aa56e'
+	}
+
+	mailer.send = function (data, template_type)
+	{
+		var email_data = _.pick(data, ['to', 'subject', 'text', 'html'])
+		var email = new sendgrid.Email(_.extend({}, defaults, email_data))
+
+		var substitutions = {}
+		var template_id = templates[template_type]
+
+		if (template_id)
+		{
+			email.setFilters(
+			{
+				templates:
+				{
+					settings:
+					{
+						enable: 1,
+						template_id: templates[template_type],
+					}
+				}
+			})
+
+			_.forEach(_.omit(data, 'text', 'html'), (value, key) =>
+			{
+				_.set(substitutions, '%' + key + '%', [ value ])
+			})
+
+			email.setSubstitutions(substitutions)
+		}
+
+		return new Promise((resolve, reject) =>
+		{
+			sendgrid.send(email, (err, response) =>
+			{
+				if (err)
+				{
+					reject(SendgridError(err))
+				}
+				else
+				{
+					resolve(response)
+				}
+			})
+		})
+	}
+
+	return mailer
+}
