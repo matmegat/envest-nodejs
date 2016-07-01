9122c762cc65120547c09da120e9b0c1a648d53e
Mat Megat <matmegat822@gmail.com>
2016-07-01 12:42:03 +0300

    (NET-733) add admins,users list route

--- src/http/api/admin/Admin.js
+++ src/http/api/admin/Admin.js
@@ -1,6 +1,7 @@
 
 var Router = require('express').Router
 var toss = require('../../toss')
+var _ = require('lodash')
 
 module.exports = function (http, admin)
 {
@@ -17,6 +18,29 @@ module.exports = function (http, admin)
 
 		toss(rs, admin.intro(target_user_id, by_user_id))
 	})
+
+	express.get('/users',  by_group('users'))
+	express.get('/admins', by_group('admins'))
+
+	function by_group (group)
+	{
+		return function (rq, rs)
+		{
+			var options = {}
+
+			options.paginator = _.pick(rq.query,
+			[
+				'page'
+			])
+
+			options.filter = _.pick(rq.query,
+			[
+				'query'
+			])
+
+			toss(rs, admin.usersList(group, options))
+		}
+	}
 
 	return ctrl
 }
