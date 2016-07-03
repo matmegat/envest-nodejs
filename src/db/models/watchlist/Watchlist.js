ac794339a8b06e1e46f3c4710f7c7cbf7fa2603c
Mat Megat <matmegat822@gmail.com>
2016-07-03 20:18:47 +0300

    fix Watchlist missing Symbols dep.

--- src/db/models/watchlist/Watchlist.js
+++ src/db/models/watchlist/Watchlist.js
@@ -8,23 +8,26 @@ var SymbolList = require('./SymbolList')
 var wrap = require('lodash/wrap')
 var pick = require('lodash/pick')
 
-var Watchlist = module.exports = function Watchlist (db, symbols)
+var Watchlist = module.exports = function Watchlist (db)
 {
 	expect(db, 'Watchlists depends on User').property('user')
 	expect(db, 'Watchlists depends on Investor').property('investor')
+	expect(db, 'Watchlists depends on Symbols').property('symbols')
 
 	var w = {}
 
 	var knex = db.knex
 
+	var symbols = db.symbols
+
 	{
-		w.user = SymbolList(knexed(knex, schema.user.table_name))
+		w.user = SymbolList(knexed(knex, schema.user.table_name), symbols)
 
 		w.user.validateId = db.user.ensure
 	}
 
 	{
-		w.investor = SymbolList(knexed(knex, schema.investor.table_name))
+		w.investor = SymbolList(knexed(knex, schema.investor.table_name), symbols)
 
 		w.investor.validateId = db.investor.ensure
 
