c06286a1154a73d3731f2ee0a4833677ca0551f8
Mat Megat <matmegat822@gmail.com>
2016-07-05 14:48:22 +0300

    Fix delete symbol from watchlist

--- src/db/models/watchlist/SymbolList.js
+++ src/db/models/watchlist/SymbolList.js
@@ -94,10 +94,15 @@ var SymbolList = module.exports = function SymbolList (table, symbols)
 		{
 			return symbols.resolve(symbol)
 		})
-		.then(() =>
+		.then((symbol) =>
 		{
 			return table()
-			.where('owner_id', owner_id)
+			.where(
+			{
+				owner_id: owner_id,
+				symbol_exchange: symbol.exchange,
+				symbol_ticker: symbol.ticker
+			})
 			.delete()
 		})
 		.then(noop)
