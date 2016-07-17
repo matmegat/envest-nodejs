714cae11095f51452a3fa1f7e7d70092459f9453
Mat Megat <matmegat822@gmail.com>
2016-07-17 15:20:49 +0300

    Db: rm symbols seed, add meta-seed structure.

---
+++ db/seeds/_symbols.js
@@ -0,0 +1,94 @@
+
+module.exports =
+[
+{
+	exchange: 'XNYS',
+	ticker:   'BABA',
+	company:  'Alibaba Group Holding Limited'
+},
+{
+	exchange: 'XNAS',
+	ticker:   'MBUU',
+	company:  'Malibu Boats Inc'
+},
+{
+	exchange: 'PINX',
+	ticker:   'CXBMF',
+	company:  'Calibre Mining Corp'
+},
+{
+	exchange: 'PINX',
+	ticker:   'EXCFF',
+	company:  'Excalibur Resources Ltd.'
+},
+{
+	exchange: 'PINX',
+	ticker:   'CABE',
+	company:  'Calibre Energy'
+},
+{
+	exchange: 'XNAS',
+	ticker:   'AAPL',
+	company:  'Apple Inc'
+},
+{
+	exchange: 'XNYS',
+	ticker:   'CMG',
+	company:  'Chipotle Mexican Grill'
+},
+{
+	exchange: 'XNAS',
+	ticker:   'COKE',
+	company:  'Coca-Cola Bottle Corp.'
+},
+{
+	exchange: 'XNAS',
+	ticker:   'FB',
+	company:  'Facebook'
+},
+{
+	exchange: 'XNAS',
+	ticker:   'NFLX',
+	company:  'Netflix Inc'
+},
+{
+	exchange: 'XNAS',
+	ticker:   'GOOG',
+	company:  'Alphabet Inc'
+},
+{
+	exchange: 'XNYS',
+	ticker:   'GE',
+	company:  'Genral Electric'
+},
+{
+	exchange: 'XNYS',
+	ticker:   'FIT',
+	company:  'Firbit Inc'
+},
+{
+	exchange: 'XNAS',
+	ticker:   'YHOO',
+	company:  'YAhoo Inc'
+},
+{
+	exchange: 'XNAS',
+	ticker:   'GRMN',
+	company:  'Garmin Ltd.'
+},
+{
+	exchange: 'XNYS',
+	ticker:   'TWTR',
+	company:  'Twitter'
+},
+{
+	exchange: 'XNAS',
+	ticker:   'ATVI',
+	company:  'Activation Blizzard'
+},
+{
+	exchange: 'XNAS',
+	ticker:   'ZG',
+	company:  'Zillow'
+}
+]
