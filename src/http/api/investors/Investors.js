c0f9cbcac9dd260651a5f1c86cb5a76b8cd82685
Mat Megat <matmegat822@gmail.com>
2016-07-27 14:48:15 +0300

    (NET-824) Split admin and regular request to investor data

--- src/http/api/investors/Investors.js
+++ src/http/api/investors/Investors.js
@@ -33,22 +33,6 @@ module.exports = function (db, http)
 		toss(rs, investors.model.public.list(options))
 	})
 
-	investors.express.get('/admin', http.adminRequired, (rq, rs) =>
-	{
-		var options = pick(rq.query,
-			[
-				'max_id',
-				'since_id',
-				'page'
-			])
-		toss(rs, investors.model.all.list(options))
-	})
-
-	investors.express.post('/featured', http.adminRequired, (rq, rs) =>
-	{
-		toss(rs, investors.model.featured.set(rq.body.investor_id))
-	})
-
 	investors.express.get('/:id', (rq, rs) =>
 	{
 		toss(rs, investors.model.public.byId(rq.params.id))
@@ -62,12 +46,6 @@ module.exports = function (db, http)
 	investors.express.get('/:id/chart', (rq, rs) =>
 	{
 		toss(rs, db.symbols.series('TSLA'))
-	})
-
-	investors.express.post('/', http.adminRequired, (rq, rs) =>
-	{
-		var data = extend({}, rq.body, { admin_id: rq.user.id })
-		toss(rs, investors.model.create(data))
 	})
 
 	investors.express.post('/:id/field', (rq, rs) =>
@@ -86,12 +64,37 @@ module.exports = function (db, http)
 		))
 	})
 
+	/***************************************************************************
+	* Admin Routes
+	* *************************************************************************/
+	investors.express.get('/admin', http.adminRequired, (rq, rs) =>
+	{
+		var options = pick(rq.query,
+			[
+				'max_id',
+				'since_id',
+				'page'
+			])
+		toss(rs, investors.model.all.list(options))
+	})
+
+	investors.express.post('/', http.adminRequired, (rq, rs) =>
+	{
+		var data = extend({}, rq.body, { admin_id: rq.user.id })
+		toss(rs, investors.model.create(data))
+	})
+
 	investors.express.post('/:id/go-public', http.adminRequired, (rq, rs) =>
 	{
 		var whom_id = rq.user.id
 		var investor_id = rq.params.id
 
 		toss(rs, investors.model.onboarding.goPublic(whom_id, investor_id))
+	})
+
+	investors.express.post('/featured', http.adminRequired, (rq, rs) =>
+	{
+		toss(rs, investors.model.featured.set(rq.body.investor_id))
 	})
 
 	return investors
