f840963e81d2d926af2e067f5b6679b40f0db4d1
Mat Megat <matmegat822@gmail.com>
2016-08-16 13:16:43 +0300

    Update: improve validator.

--- src/db/models/feed/Update.js
+++ src/db/models/feed/Update.js
@@ -33,11 +33,12 @@ module.exports = function Update (db)
 		}
 	})
 
+
+	var PostPicNotFound = Err('post_pic_not_found',
+		'No corresponding picture for this hash')
+
 	function validate_update (data)
 	{
-		var PostPicNotFound = Err('post_pic_not_found',
-			'Post Pic Not Found')
-
 		var data = pick(data,
 		[
 			'symbols',
@@ -46,35 +47,38 @@ module.exports = function Update (db)
 			'pic'
 		])
 
-		validate.required(data.text, 'text')
-		validate.empty(data.text, 'text')
+		return new Promise(rs =>
+		{
+			validate.required(data.text, 'text')
+			validate.empty(data.text, 'text')
 
-		validate.required(data.title, 'title')
-		validate.empty(data.title, 'title')
+			validate.required(data.title, 'title')
+			validate.empty(data.title, 'title')
 
-		validate.required(data.symbols, 'symbols')
-		validate.empty(data.symbols, 'symbols')
-		validate.array(data.symbols, 'symbols')
+			validate.required(data.symbols, 'symbols')
+			validate.empty(data.symbols, 'symbols')
+			validate.array(data.symbols, 'symbols')
 
-		validate.empty(data.pic, 'pic')
+			validate.empty(data.pic, 'pic')
 
-		return Promise.resolve()
-		.then(() =>
+			rs(data)
+		})
+		.then(data =>
 		{
 			if (data.pic)
 			{
 				return db.static.exists(data.pic)
-				.then(is_exists =>
+				.then(so =>
 				{
-					if (! is_exists)
+					if (! so)
 					{
 						throw PostPicNotFound({ hash: data.pic })
 					}
+
+					return data
 				})
 			}
-		})
-		.then(() =>
-		{
+
 			return data
 		})
 	}
