012ef9b3751e63e8467243756da817e168b426c4
Mat Megat <matmegat822@gmail.com>
2016-09-02 00:11:38 +0300

    Extend holdings.isDateAvail with symbol

--- src/db/models/investor/Portfolio/Holdings.js
+++ src/db/models/investor/Portfolio/Holdings.js
@@ -114,13 +114,25 @@ module.exports = function Holdings (db, investor, portfolio)
 	})
 
 	holdings.isDateAvail =
-		knexed.transact(knex, (trx, investor_id, for_date) =>
+		knexed.transact(knex, (trx, investor_id, for_date, symbol) =>
 	{
+		if (symbol)
+		{
+			symbol = Symbl(symbol)
+			console.log(symbol, symbol.toXign())
+		}
+
 		return investor.all.ensure(investor_id, trx)
 		.then(() =>
 		{
+			var where = { investor_id: investor_id }
+			if (symbol)
+			{
+				extend(where, symbol.toDb())
+			}
+
 			return table(trx)
-			.where('investor_id', investor_id)
+			.where(where)
 			.andWhere('timestamp', '>', for_date)
 		})
 		.then(res =>
