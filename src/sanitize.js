e2309a94a7fcdaa89c53a6e4e7a12d32d922412f
Mat Megat <matmegat822@gmail.com>
2016-09-14 16:37:06 +0300

    fix allowed tags and attributes

--- src/sanitize.js
+++ src/sanitize.js
@@ -6,8 +6,10 @@ var sanitize = module.exports = {}
 sanitize.brief = function sanitize_brief (string)
 {
 	return sanitizeHTML(string, {
-		allowedTags: ['p', 'b', 'i', 'em', 'strong', 'br'],
-		allowedAttributes: [],
+		allowedTags: ['ul', 'li', 'a', 'em', 'strong', 'u' 'br'],
+		allowedAttributes: {
+			a: ['href']
+		},
 		selfClosing: ['br'],
 		parser: {
 			lowerCaseTags: true
