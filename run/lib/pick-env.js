0294b46498c9ea5ba59e6c03a4c06b2647e93f91
Mat Megat <matmegat822@gmail.com>
2016-09-27 15:34:11 +0300

    add prod address.

--- run/lib/pick-env.js
+++ run/lib/pick-env.js
@@ -13,7 +13,7 @@ module.exports = (env) =>
 
 var remotes = {}
 remotes.devs = 'ec2-52-38-31-214.us-west-2.compute.amazonaws.com'
-remotes.prod = 'ec2-52-38-31-214.us-west-2.compute.amazonaws.com'
+remotes.prod = 'ec2-52-37-60-63.us-west-2.compute.amazonaws.com'
 
 var envs =
 {
@@ -31,7 +31,7 @@ var envs =
 	},
 	prod:
 	{
-		remote: remotes.devs,
+		remote: remotes.prod,
 		confirm: true
 	},
 	virtual:
