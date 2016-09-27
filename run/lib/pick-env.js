6cfb53f96b5698841a2f22c2093b08e8bf64e6eb
Mat Megat <matmegat822@gmail.com>
2016-09-27 20:10:15 +0300

    envs.

--- run/lib/pick-env.js
+++ run/lib/pick-env.js
@@ -11,31 +11,4 @@ module.exports = (env) =>
 	}
 }
 
-var remotes = {}
-remotes.devs = 'ec2-52-38-31-214.us-west-2.compute.amazonaws.com'
-remotes.prod = 'ec2-52-37-60-63.us-west-2.compute.amazonaws.com'
-
-var envs =
-{
-	dev:
-	{
-		remote: remotes.devs
-	},
-	test:
-	{
-		remote: remotes.devs
-	},
-	staging:
-	{
-		remote: remotes.devs
-	},
-	prod:
-	{
-		remote: remotes.prod,
-		confirm: true
-	},
-	virtual:
-	{
-		remote: '172.17.0.2'
-	}
-}
+var envs = require('./envs')
