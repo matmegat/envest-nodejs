4ebed75fc09f2b471d602bd4f0ddd0e88b63c126
Mat Megat <matmegat822@gmail.com>
2016-10-14 19:53:04 +0300

    improve TradeOp.

--- src/db/models/investor/Portfolio/TradeOp/TradeOp.js
+++ src/db/models/investor/Portfolio/TradeOp/TradeOp.js
@@ -1,25 +1,41 @@
 
 var assign = Object.assign
 
-var Op = require('./Op')
-
+var expect = require('chai').expect
 var wrap = require('lodash/wrap')
+
+var Op = require('./Op')
+var Symbl = require('../../../symbols/Symbl')
 
 module.exports = function TradeOp (investor_id, timestamp, trade_data)
 {
 	var op = Op(investor_id, timestamp)
 
+	expect(trade_data).an('object')
+
+	expect(trade_data).property('dir')
+	expect([ 'sold', 'bought' ]).include(trade_data.dir)
+
+	expect(trade_data.price).a('number')
+	expect(trade_data.amount).a('number')
+
+	expect(trade_data.symbol).ok
+
 	op.type = 'trade'
 
-	//
-	op.trade_data = trade_data
+	op.trade_data = {}
+
+	op.trade_data.dir = trade_data.dir
+	op.trade_data.price = trade_data.price
+	op.trade_data.amount = trade_data.amount
+	op.trade_data.symbol = Symbl(trade_data.symbol)
 
 	op.toDb = wrap(op.toDb, toDb =>
 	{
 		return assign(toDb(),
 		{
-			type:  op.type,
-			trade: op.trade_data
+			type: op.type,
+			trade_data: op.trade_data
 		})
 	})
 
