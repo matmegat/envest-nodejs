00128542c88bfd0d17035d671903f7b14aa0d03c
Mat Megat <matmegat822@gmail.com>
2016-10-17 17:28:11 +0300

    Non Trade Operation

--- src/db/models/investor/Portfolio/TradeOp/NonTradeOp.js
+++ src/db/models/investor/Portfolio/TradeOp/NonTradeOp.js
@@ -1,6 +1,58 @@
 
+var assign = Object.assign
+
+var expect = require('chai').expect
+var wrap   = require('lodash/wrap')
+
+var Op = require('./Op')
+
 module.exports = function NonTradeOp (investor_id, timestamp, op_data)
 {
-	// must be compatible with Op
-	// must have methods compatible with Op
+	var non_trade_operations =
+	[
+		'deposit',
+		'withdrawal',
+		'interest',
+		'fee',
+
+		'init',
+	]
+
+	var op = Op(investor_id, timestamp)
+
+	expect(op_data).to.be.an('object')
+
+	expect(op_data).property('type')
+	expect(non_trade_operations).include(op_data.type)
+
+	expect(op_data.amount).to.be.a('number')
+
+	op.type = 'npn-trade'
+
+	op.op_data = {}
+	op.op_data.type = op.type
+	op.op_data.amount = op.amount
+
+	op.toDb = wrap(op.toDb, (toDb) =>
+	{
+		return assign(toDb(),
+		{
+			type: op.type,
+			data: op.op_data
+		})
+	})
+
+	op.apply = (trx, portfolio) =>
+	{
+		// TODO
+		return Promise.resolve()
+	}
+
+	op.undone = (trx, portfolio) =>
+	{
+		// TODO
+		return Promise.resolve()
+	}
+
+	return op
 }
