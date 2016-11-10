1ec79ae19f99c7d4df99ed4b4c5031f66fddc63f
Mat Megat <matmegat822@gmail.com>
2016-11-10 15:24:09 +0300

    remove email confirm template for admin

--- src/db/models/User.js
+++ src/db/models/User.js
@@ -543,29 +543,10 @@ module.exports = function User (db, app)
 				+ `Confirm Email</a><br>`
 				+ `Your email confirm code: <strong>${code.toUpperCase()}</strong>`
 			}
-		},
-		admin: function (host, user, code)
-		{
-			expect('host').to.be.a('string')
-			expect('email').to.be.a('string')
-			expect('code').to.be.a('string')
-
-			return {
-				to: user.email,
-				subject: 'Reset Password',
-				html: `Hi, ${user.first_name} ${user.last_name}.`
-				+ `<br/><br/>`
-				+ `You've been chosen as admin.<br/>`
-				+ `Please tap the link to reset password: `
-				+ `<a href="http://${host}/reset-password?code=`
-				+ `${code.toUpperCase()}" target="_blank">`
-				+ `Reset Password</a><br>`
-				+ `Your email confirm code: <strong>${code.toUpperCase()}</strong>`
-			}
 		}
 	}
 
-	user.newEmailUpdate = function (trx, data, for_admin)
+	user.newEmailUpdate = function (trx, data)
 	{
 		data = extend({}, data, { new_email: data.new_email.toLowerCase() })
 
@@ -612,19 +593,7 @@ module.exports = function User (db, app)
 					email_title: [ 'Confirm Email' ]
 				}
 
-				if (for_admin)
-				{
-					substs =
-					{
-						email_title: [ 'Reset Password' ]
-					}
-
-					mail_content = email_templates.admin(host, user_item, code)
-				}
-				else
-				{
-					mail_content = email_templates.user(host, user_item, code)
-				}
+				mail_content = email_templates.user(host, user_item, code)
 
 				return mailer.send('default', substs, mail_content)
 				.then(() => user_item.id, console.err)
