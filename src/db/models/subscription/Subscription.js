9a6e9529773d57d0eb0aef30b7de476e5cb7577b
Mat Megat <matmegat822@gmail.com>
2016-11-22 17:32:41 +0300

    tidy style

--- src/db/models/subscription/Subscription.js
+++ src/db/models/subscription/Subscription.js
@@ -205,16 +205,16 @@ module.exports = function NetvestSubsc (db, cfg, mailer)
 	netvest_subscr.countBySubscriptions = () =>
 	{
 		var result = {}
-		var users_subscriptions_table = db.user.usersSubscriptions
+		var users_subscr_table = db.user.usersSubscriptions
 
-		return count(users_subscriptions_table()
+		return count(users_subscr_table()
 		.where('created_at', '>', moment().subtract(1, 'month'))
 		.whereNull('subscriptions.user_id'))
 		.then(trial_count =>
 		{
 			result.trial = trial_count
 
-			return count(users_subscriptions_table()
+			return count(users_subscr_table()
 			.where('created_at', '<=', moment().subtract(1, 'month'))
 			.whereNull('subscriptions.user_id'))
 		})
@@ -222,7 +222,7 @@ module.exports = function NetvestSubsc (db, cfg, mailer)
 		{
 			result.standard = standard_count
 
-			return count(users_subscriptions_table()
+			return count(users_subscr_table()
 			.whereNotNull('subscriptions.user_id'))
 		})
 		.then(premium_count =>
